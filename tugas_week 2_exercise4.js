dedec695802ad361b8dd21ee050b8329183787d6
KadeAK <48665932+KadeAK@users.noreply.github.com>
2019-03-26 23:08:38 +0700

    Update
    Adding switch and case

--- tugas_week 2_exercise4.js
+++ tugas_week 2_exercise4.js
@@ -1,5 +1,6 @@
-var tanggal= Math.floor(Math.random ()*31);
-var bulan = Math.floor(Math.random ()*12);
+var tanggal= Math.floor(Math.random ()*31)+1;
+var bulan = Math.floor(Math.random ()*12)+1;
+var namaBulan = '';
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(1900);
@@ -7,4 +8,43 @@ function getRandomIntInclusive(min, max) {
     var tahun = Math.floor(Math.random() * (1900 - 2200 + 1)) + 2200; 
     //The maximum is inclusive and the minimum is inclusive 
 
-    console.log(tanggal, bulan, tahun);
+switch (bulan){
+    case 1 :
+        namaBulan = 'January';
+        break;
+    case 2 :
+        namaBulan = 'Febuary';
+        break;
+    case 3 :
+        namaBulan = 'Maret';
+        break;
+    case 4 :
+        namaBulan = 'April' ;
+        break;
+    case 5 :
+        namaBulan = 'Mei' ;
+        break;
+    case 6 :
+        namaBulan = 'Juni' ;
+        break;
+    case 7 :
+        namaBulan = 'July' ;
+        break;
+    case 8 :
+        namaBulan = 'Agustus' ;
+        break;
+    case 9 :
+        namaBulan = 'September' ;
+        break;
+    case 10 :
+        namaBulan = 'Oktober' ;
+        break;
+    case 11 :
+        namaBulan = 'November' ;
+        break;
+    case 12 :
+        namaBulan = 'Desember';
+    break;
+}
+
+    console.log(tanggal, namaBulan, tahun);
