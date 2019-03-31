ca50fce432141129fb1aa947b7cdc2ad40d1fa0a
KadeAK <48665932+KadeAK@users.noreply.github.com>
2019-03-31 22:15:24 +0700

    Update tugas_week2_exercise7.js

--- tugas_week2_exercise7.js
+++ tugas_week2_exercise7.js
@@ -21,6 +21,13 @@ for (var j = 0 ; j < 5 ; j++)
 }
 
 //Nomer 3
+for(var i = 0; i < 5; i++){
+for(var j = 0; j <= 0; j++)
+  asterix += '*';
+  console.log(asterix)
+}
+
+//cara lain
 var asterix = ''; 
 
 for(var i = 0; i < 5; i++){
